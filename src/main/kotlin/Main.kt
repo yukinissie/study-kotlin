f2ac58f9a0d4bb49562a5f84bcbe3f64f85fe1f8
yukinissie <bigtree2490@outlook.jp>
2022-05-21 12:02:40 +0900

    文字列テンプレートを修正

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
     val question = "The Question"
     val answer = 20
-    println("Question: ${question}\nAnswer: $answer")
+    println("Question: $question\nAnswer: $answer")
 }
 
 // def max(a, b) a > b ? a : b end
