0f2f5bb5eabe250e1712c6274788ba29628d2342
yukinissie <bigtree2490@outlook.jp>
2022-05-21 10:55:28 +0900

    Add max fun

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,11 @@
 fun main(args: Array<String>) {
     println("Hello World!")
+    println(max(10, 100))
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 }
+
+// def max(a, b) a > b ? a : b end
+fun max(a: Int, b: Int) = if (a > b) a else b
